367b0e636ceb24f1d9b4f1f20b793dc4fa80657a
Victor De León <victordeleon@lefuse.com>
2020-10-25 13:28:48 -0700

    adds responsive menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
+let $body = $("body");
+let $toggle_menu_button = $(".navTrigger");
+let $nav_menu = $(".nav");
 
+// Toggle Mobile Menu
+$toggle_menu_button.on("click", function() {
+  $nav_menu.toggleClass("activated");
+  $body.toggleClass("no-scroll");
+  $(this).toggleClass("active");
+});
+
+// scripts for slick library
 $('.slick-container').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -16,6 +27,7 @@ $('.slider-nav').slick({
   focusOnSelect: true
 });
 
+//script for view more card
 $('.view-more-card').click(function(event) {
   event.preventDefault();
   let cardID = this.getAttribute('data-target');
